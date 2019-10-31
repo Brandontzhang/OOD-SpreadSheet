58f111b390cb7e5591b09656f0f2909ddf825186
JoshGoldsmith <joshgoldsmith5793@gmail.com>
2019-10-31 18:33:10 -0400

    Fixed Javastyle

--- test/cs3500/worksheet/CellTest.java
+++ test/cs3500/worksheet/CellTest.java
@@ -15,41 +15,41 @@ public class CellTest {
     Parser cellParser = new Parser();
     assertEquals("2.0", cellParser.parse("2").toString());
   }
-//
-//
-//  // Creating Cell Tests ------------------------------------------------------------------------
-//  @Test
-//  public void createCellTest() {
-//    WorkSheet w1
-//    Cell c1 = new Cell();
-//    assertEquals("", c1.viewCell());
-//  }
-//
-//  @Test
-//  public void createCellTest2() {
-//    Cell c1 = new Cell(2);
-//    assertEquals("2", c1.viewCell());
-//  }
-//
-//  @Test
-//  public void createCellTest3() {
-//    Cell c1 = new Cell(true);
-//    Cell c2 = new Cell(false);
-//    assertEquals("true", c1.viewCell());
-//    assertEquals("false", c2.viewCell());
-//  }
-//
-//  @Test
-//  public void createCellTest4() {
-//    // symbol
-//    Cell c1 = new Cell("Hello");
-//    assertEquals("Hello", c1.viewCell());
-//  }
-//
-//  @Test
-//  public void createCellTest5() {
-//    // string
-//    Cell c1 = new Cell("\"hi\"");
-//    assertEquals("\"hi\"", c1.viewCell());
+  //
+  //
+  //  // Creating Cell Tests ---------------------------------------------------------------------
+  //  @Test
+  //  public void createCellTest() {
+  //    WorkSheet w1
+  //    Cell c1 = new Cell();
+  //    assertEquals("", c1.viewCell());
+  //  }
+  //
+  //  @Test
+  //  public void createCellTest2() {
+  //    Cell c1 = new Cell(2);
+  //    assertEquals("2", c1.viewCell());
+  //  }
+  //
+  //  @Test
+  //  public void createCellTest3() {
+  //    Cell c1 = new Cell(true);
+  //    Cell c2 = new Cell(false);
+  //    assertEquals("true", c1.viewCell());
+  //    assertEquals("false", c2.viewCell());
+  //  }
+  //
+  //  @Test
+  //  public void createCellTest4() {
+  //    // symbol
+  //    Cell c1 = new Cell("Hello");
+  //    assertEquals("Hello", c1.viewCell());
+  //  }
+  //
+  //  @Test
+  //  public void createCellTest5() {
+  //    // string
+  //    Cell c1 = new Cell("\"hi\"");
+  //  assertEquals("\"hi\"", c1.viewCell());
   //}
 }
